6037711e29aa13989647eae3682a4df8667c43b1
Ashok <ashok@codecraft.co.in>
2017-12-08 22:06:52 +0530

    gitignore file and updates

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const API_KEY = '993a4b1bd0940005fb9bef7a66b5c1b1';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather(city){
+export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
-
-    return{
+    console.log(request);
+    return {
         type: FETCH_WEATHER,
-        payload: request        
+        payload: request
     }
 }
